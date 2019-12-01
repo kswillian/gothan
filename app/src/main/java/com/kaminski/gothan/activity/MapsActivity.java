825ec05928c744a9fcc34026f06b25980de2cae6
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-12-01 13:20:04 -0300

    Implementando a busca da localização do usuário

--- app/src/main/java/com/kaminski/gothan/activity/MapsActivity.java
+++ app/src/main/java/com/kaminski/gothan/activity/MapsActivity.java
@@ -1,27 +1,54 @@
 package com.kaminski.gothan.activity;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.kaminski.gothan.R;
 import com.kaminski.gothan.firebase.Firebase;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FloatingActionButton floatingActionButtonLocation;
+    private com.github.clans.fab.FloatingActionMenu floatingActionButtonInfo;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private double latitude = 0;
+    private double longitude = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        firebaseAuth = Firebase.getFirebaseAuth();
+        initComponent();
+        initEvent();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -32,9 +59,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locateUser();
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void initComponent() {
+        floatingActionButtonLocation = findViewById(R.id.floatingActionButtonMapLocation);
+        floatingActionButtonInfo = findViewById(R.id.fab);
+
+        floatingActionButtonLocation.setVisibility(View.INVISIBLE);
+        floatingActionButtonInfo.setVisibility(View.INVISIBLE);
+    }
+
+    public void initEvent(){
+
+        floatingActionButtonLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveCameraToUserLocation();
+            }
+        });
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void visibilityFloatActions(){
+
+        if (latitude != 0) {
+            floatingActionButtonLocation.setVisibility(View.VISIBLE);
+            floatingActionButtonInfo.setVisibility(View.VISIBLE);
+        }else{
+            floatingActionButtonLocation.setVisibility(View.INVISIBLE);
+            floatingActionButtonInfo.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void locateUser() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+                LatLng myLocal = new LatLng(latitude, longitude);
+
+                mMap.clear();
+                visibilityFloatActions();
+                mMap.addMarker(new MarkerOptions().position(myLocal).title("Eu estou aqui"));
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    0,
+                    locationListener
+            );
+        }
+    }
+
+    public void moveCameraToUserLocation(){
+
+        LatLng coordinate = new LatLng(latitude, longitude);
+
+        CameraUpdate myLocation = CameraUpdateFactory.newLatLngZoom(
+                coordinate, 15);
+        mMap.animateCamera(myLocation);
     }
 }
