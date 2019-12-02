2640268fb3857a8ea5925b7aa000251585ce7b4c
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-12-02 02:13:06 -0300

    Implementando o controle de Geologalização e Notificações

---
+++ app/src/main/java/com/kaminski/gothan/util/UpdateGlobalLocal.java
@@ -0,0 +1,29 @@
+package com.kaminski.gothan.util;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.kaminski.gothan.firebase.Firebase;
+
+public class UpdateUser {
+
+    public static void updatelocation(String id, double lat, double lgt){
+
+        DatabaseReference locationUser = Firebase.getFirebase().child("location_user");
+        GeoFire geoFire = new GeoFire(locationUser);
+
+        geoFire.setLocation(
+                id,
+                new GeoLocation(lat, lgt),
+                new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+                        if(error != error){
+                            System.out.println(":::::::::::::::::: ERRO");
+                        }
+                    }
+                }
+        );
+    }
+}
