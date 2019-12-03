3ae9bc57bb9f347f77efbad30d871ad0ee828565
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-12-03 02:43:44 -0300

    Implementando a listagem e remoção das ocorrências do usuário

--- app/src/main/java/com/kaminski/gothan/adapter/Adapter.java
+++ app/src/main/java/com/kaminski/gothan/adapter/Adapter.java
@@ -1,4 +1,63 @@
 package com.kaminski.gothan.adapter;
 
-public class Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kaminski.gothan.R;
+import com.kaminski.gothan.model.Ocurrence;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
+
+    private List<Ocurrence> ocurrenceList;
+    private Context context;
+
+    public Adapter(List<Ocurrence> ocurrenceList, Context context) {
+        this.ocurrenceList = ocurrenceList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_list_acurrences, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Ocurrence ocurrence = ocurrenceList.get(position);
+        holder.icon.setImageResource(R.mipmap.ic_launcher);
+        holder.type.setText(ocurrence.getType());
+        holder.description.setText(ocurrence.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return ocurrenceList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView icon;
+        TextView description;
+        TextView type;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            icon = itemView.findViewById(R.id.imageViewOcurence);
+            type = itemView.findViewById(R.id.textViewTypeOcurrence);
+            description = itemView.findViewById(R.id.textViewDescriptionOcurrence);
+        }
+    }
 }
--- app/src/main/java/com/kaminski/gothan/fragment/OcurrencesFragment.java
+++ app/src/main/java/com/kaminski/gothan/fragment/OcurrencesFragment.java
@@ -1,22 +1,166 @@
 package com.kaminski.gothan.fragment;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.kaminski.gothan.R;
+import com.kaminski.gothan.activity.MainActivity;
+import com.kaminski.gothan.adapter.Adapter;
+import com.kaminski.gothan.firebase.Firebase;
+import com.kaminski.gothan.model.Ocurrence;
+import com.kaminski.gothan.util.Base64Custom;
+import com.kaminski.gothan.util.RecyclerItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OcurrencesFragment extends Fragment {
+
+    private TextView textViewOcurrenceEmpty;
+    private RecyclerView recyclerViewOcurrences;
+    private List<Ocurrence> ocurrenceList = new ArrayList<>();
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private Adapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ocurrences, container, false);
+        View view = inflater.inflate(R.layout.fragment_ocurrences, container, false);
+
+        firebaseAuth = Firebase.getFirebaseAuth();
+        databaseReference = Firebase.getFirebase().child("ocurrences");
+
+        initCompenent(view);
+        configRecycler(view);
+        findOcurrences();
+        initEvent();
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        isEmpty();
+        super.onResume();
+    }
+
+    public void initCompenent(View view){
+        textViewOcurrenceEmpty = view.findViewById(R.id.textViewOcurrenceEmpty);
+        recyclerViewOcurrences = view.findViewById(R.id.recyclerViewOcurrences);
+    }
+
+    public void configRecycler(View view){
+        adapter = new Adapter(ocurrenceList, view.getContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerViewOcurrences.setLayoutManager(layoutManager);
+        recyclerViewOcurrences.setHasFixedSize(true);
+        recyclerViewOcurrences.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayout.HORIZONTAL));
+        recyclerViewOcurrences.setAdapter(adapter);
+    }
+
+    public void initEvent(){
+
+        recyclerViewOcurrences.addOnItemTouchListener(new RecyclerItemClickListener(
+                getActivity(),
+                recyclerViewOcurrences,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+
+                        final Ocurrence ocurrence = ocurrenceList.get(position);
+
+                        AlertDialog.Builder msg = new AlertDialog.Builder(getContext());
+                        msg.setTitle(getResources().getString(R.string.alert_del_ocu_title));
+                        msg.setMessage(getResources().getString(R.string.alert_del_ocu_desc));
+                        msg.setPositiveButton(getResources().getString(R.string.alert_del_ocu_button_ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                deleteOcurrence(ocurrence.getId());
+                            }
+                        });
+                        msg.setNegativeButton(getResources().getString(R.string.alert_del_ocu_button_cancel), null);
+                        msg.show();
+
+                    }
+
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    }
+                }
+        ));
+    }
+
+    public void findOcurrences(){
+
+        Query query = databaseReference.
+                orderByChild("userId").
+                equalTo(Base64Custom.encodeBase64(firebaseAuth.getCurrentUser().getEmail()));
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    ocurrenceList.add(ds.getValue(Ocurrence.class));
+                }
+                adapter.notifyDataSetChanged();
+
+                isEmpty();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        isEmpty();
+    }
+
+    public void deleteOcurrence(String ocurrenceId){
+
+        DatabaseReference databaseRef = Firebase.getFirebase();
+        DatabaseReference databaseReferenceGlobal = databaseRef.child("location_global");
+        DatabaseReference databaseReferenceOcurrence = databaseRef.child("ocurrences");
+        databaseReferenceGlobal.child(ocurrenceId).removeValue();
+        databaseReferenceOcurrence.child(ocurrenceId).removeValue();
+        ocurrenceList.clear();
+        isEmpty();
+        findOcurrences();
+    }
+
+    public void isEmpty(){
+        if(!ocurrenceList.isEmpty())
+           textViewOcurrenceEmpty.setVisibility(View.INVISIBLE);
+    }
 }
